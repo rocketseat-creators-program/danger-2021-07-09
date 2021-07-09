58262ad5f456a531524a0270e5eb61ef8b4952dd
Eduardo Koller <eduardo@bossabox.com>
2021-07-09 10:47:44 -0300

    ci: new ci pipeline for danger

--- dangerfile.ts
+++ dangerfile.ts
@@ -7,6 +7,8 @@ const controllerFiles = danger.git.fileMatch("src/controllers/**");
 const routeFiles = danger.git.fileMatch("src/routes/**");
 const docs = danger.git.fileMatch("auth.apib");
 
+const changelog = danger.git.fileMatch("CHANGELOG.md");
+
 if(serviceFiles.edited && !serviceTestFiles.edited) {
     warn("Existem modificações em arquivos de serviço, mas não existem modificações em arquivos de teste. Isto é OK desde que o código esteja sendo refatorado.");
 }
@@ -15,3 +17,6 @@ if((controllerFiles.edited || routeFiles.edited) && !docs.edited) {
     fail(":exclamation: Houveram mudanças em arquivos de controllers/rotas, mas a documentação não foi alterada!");
 }
 
+if(!changelog.edited) {
+    warn("Não foram detectadas mudanças no changelog. Por favor, verifique as nossas regras sobre CHANGELOG [aqui](https://google.com).")
+}
